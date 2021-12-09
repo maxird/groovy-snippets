15b002c716745cd3ff583f99f28dcec624898956
ryan <ryanvaniderstine@maximus.com>
2021-12-09 09:20:10 -0400

    more

--- vars/spfTagVersion.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-    sh("""
-        echo "zzz: I am here PWD[\${PWD}]"
-        env | sort
-        sed -i -e "s/0.0.0/\${BUILD_DISPLAY_NAME}/g" .spf/spf.yml
-        cat .spf/spf.yml
-        curl -s ifconfig.io
-    """)
-}
