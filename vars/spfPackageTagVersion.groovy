a6ad36a974d25a67fe71b8797812c17bd214aad4
ryan <ryanvaniderstine@maximus.com>
2021-12-09 09:22:39 -0400

    more

--- vars/spfPackageTagVersion.groovy
+++ vars/spfPackageTagVersion.groovy
@@ -1,6 +1,6 @@
 def call() {
     sh("""
-        echo "spfPackageTagVersion -> spf.yml -> \${BUILD_DISPLAY_NAME}"
+        echo "spfPackageTagVersion -> .spf/spf.yml -> \${BUILD_DISPLAY_NAME}"
         sed -i -e "s/0.0.0/\${BUILD_DISPLAY_NAME}/g" .spf/spf.yml
         curl -s ifconfig.io
     """)
